89a166e478ad6bd2956900ee27b67e8b0b04f571
qunqun.xu 许群群 <qunqun.xu@qiyuesuo.com>
2020-02-11 21:43:06 +0800

    序列化方式替换, 集群连接, clusterIp/nginx stream proxy

--- src/test/java/com/github/xuqplus2/hello/redis/DemoApplicationTests.java
+++ src/test/java/com/github/xuqplus2/hello/redis/DemoApplicationTests.java
@@ -6,39 +6,54 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.security.SecureRandom;
 
 @SpringBootTest
 @Slf4j
 class DemoApplicationTests {
 
-    @Autowired
-    RedisTemplate redisTemplate;
+	@Autowired
+	RedisTemplate redisTemplate;
 
-    @Test
-    void contextLoads() throws IOException {
-        System.getProperties().put("socksProxySet", "true");
-        System.getProperties().put("socksProxyHost", "localhost");
-        System.getProperties().put("socksProxyPort", "9999");
-        Process exec = Runtime.getRuntime().exec("ping 10.244.1.12");
-        OutputStream os = exec.getOutputStream();
-        InputStream is = exec.getInputStream();
-        InputStream es = exec.getErrorStream();
-        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(es, "gbk"))) {
-            String line = null;
-            while (null != (line = bufferedReader.readLine())) {
-                log.info(line);
-            }
-        }
-        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(is, "gbk"))) {
-            String line = null;
-            while (null != (line = bufferedReader.readLine())) {
-                log.info(line);
-            }
-        }
+	@Test
+	void contextLoads() throws IOException {
+		long millis = System.currentTimeMillis();
+		SecureRandom secureRandom = new SecureRandom();
 
-        long millis = System.currentTimeMillis();
-        redisTemplate.boundValueOps("key" + millis).set("value" + millis);
-    }
+		for (int i = 0; i < 100; i++) {
+			String key = "key-" + i;
+			redisTemplate.boundValueOps(key).set("value" + millis);
+			Object result = redisTemplate.boundValueOps(key).get();
+			log.info("i=>{}, key=>{}, result=>{}, ", i, key, result);
+//			redisTemplate.delete("key");
+		}
+	}
+
+	private void f() throws IOException {
+		System.getProperties().put("socksProxySet", "true");
+		System.getProperties().put("socksProxyHost", "localhost");
+		System.getProperties().put("socksProxyPort", "9999");
+		Process exec = Runtime.getRuntime().exec("ping 10.244.1.12");
+		OutputStream os = exec.getOutputStream();
+		InputStream is = exec.getInputStream();
+		InputStream es = exec.getErrorStream();
+		try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(es, "gbk"))) {
+			String line = null;
+			while (null != (line = bufferedReader.readLine())) {
+				log.info(line);
+			}
+		}
+		try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(is, "gbk"))) {
+			String line = null;
+			while (null != (line = bufferedReader.readLine())) {
+				log.info(line);
+			}
+		}
+	}
 
 }
